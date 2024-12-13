342037c9ebe7200c23a441092bfffba8b214d5d7
faiyted <faiyted@gmai.com>
2024-12-13 11:11:41 -0600

    updating html and img

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Step 2  pip install flask pymongo pandas
 
 # Initialize Flask app
-app = Flask(__name__)
+app = Flask(__name__,static_folder="iframe_figures")
 
 # MongoDB connection
 client = MongoClient(os.getenv("mongodb+srv://group_user:UTProj4@project4.ofsuk.mongodb.net/remote?retryWrites=true&w=majority"))
@@ -19,18 +19,20 @@ collection = db["records"]
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # figures = ["figure_73.html"]  # Add all figure filenames dynamically
+    return render_template("index.html")
 
-@app.route('/mongo_data', methods=['GET'])
-def mongo_data():
-    try:
-        # Fetch data from MongoDB collection
-        data = list(collection.find({}, {'_id': 0}))  # Exclude the MongoDB `_id` field if not needed
 
-        # Convert MongoDB cursor to JSON
-        return jsonify(data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+# @app.route('/mongo_data', methods=['GET'])
+# def mongo_data():
+#     try:
+#         # Fetch data from MongoDB collection
+#         data = list(collection.find({}, {'_id': 0}))  # Exclude the MongoDB `_id` field if not needed
+
+#         # Convert MongoDB cursor to JSON
+#         return jsonify(data), 200
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 
 # Run the Flask app
 if __name__ == '__main__':
